b963ba6ec6ff2f95e36e5a84b98114ea8e7d8cad
Melchipaul <ndumelchipaul2000@gmail.com>
2023-09-24 23:08:48 +0200

    Admin

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -10,11 +10,8 @@ class Book(models.Model):
         validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
-    slug = models.SlugField(default="", null=False, db_index=True) # harry-potter-1
+    slug = models.SlugField(default="", blank=True, null=False, db_index=True) # harry-potter-1
     
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
     
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
